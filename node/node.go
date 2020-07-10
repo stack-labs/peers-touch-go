f2615129d0a447f158ed1a41262f970de68706ee
shu xian <i@shux.me>
2020-07-10 09:07:19 +0800

    node struct begin

--- node/node.go
+++ node/node.go
@@ -1,1 +1,21 @@
 package node
+
+import (
+	"sync"
+
+	"github.com/ipfs/go-ipfs/core/coreapi"
+)
+
+type node struct {
+	ipfs coreapi.CoreAPI
+
+	muPubSub sync.RWMutex
+	muIPFS   sync.RWMutex
+}
+
+func (n *node) IPFS() coreapi.CoreAPI {
+	n.muIPFS.RLock()
+	defer n.muIPFS.RUnlock()
+
+	return n.ipfs
+}
