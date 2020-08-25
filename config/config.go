cc9d083c23ffe59f8de140b72f92ed37bcaa1414
shu xian <i@shux.me>
2020-08-25 09:26:17 +0800

    config begin

--- config/config.go
+++ config/config.go
@@ -1,19 +1,37 @@
 package config
 
+import "sync"
+
 var (
 	DefaultStartPath = ""
+	DefaultSeparator = "."
+	mux              sync.Mutex
+	configs          = &allConfigs{
+		values: map[string]Options{},
+	}
 )
 
 type allConfigs struct {
-	values 
+	values map[string]Options
 }
-
 
 type Options interface {
 	Path() string
-	Parse(options Options)
 }
 
-func Parse(options Options) {
-	configs := allConfigs.
+type AbstractOptions struct {
+	Subs map[string]Options
+}
+
+func (a *AbstractOptions)
+
+func Register(options Options) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	configs.values[options.Path()] = options
+}
+
+func parse() {
+
 }
